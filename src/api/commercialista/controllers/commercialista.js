32a1f18ce2ef72d07d0b8147a613868521ddfd28
Gianfranco Maffei <maffei.gianfranco@gmail.com>
2025-03-22 14:40:24 +0100

    first collection types fix

---
+++ src/api/commercialista/controllers/commercialista.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * commercialista controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::commercialista.commercialista');
---
+++ src/api/commercialista/routes/commercialista.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * commercialista router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::commercialista.commercialista');
